d4a87604663db6ee1fce74b15434bb465ee4e3ea
jarednapier <46578158+jarednapier@users.noreply.github.com>
2019-07-21 14:53:15 -0400

    Project setup.

---
+++ src/JS/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../CSS/App.css';
+
+function App() {
+  return (
+    <div>
+      Thanks for makin me a fighter oh oh
+    </div>
+  );
+}
+
+export default App;
