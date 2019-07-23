90e5a71abcecee2ab48cf7f6def87555c8ea0235
jarednapier <46578158+jarednapier@users.noreply.github.com>
2019-07-22 21:20:16 -0400

    Finishes toggle setup.

--- src/JS/Toggle.js
+++ src/JS/Toggle.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../CSS/Toggle.css';
+import ColoredSquare from './ColoredSquare';
 
-const Toggle = () => {
+const Toggle = (props) => {
+    return (
+        <div className="wrapper"> 
+            <p className="title">{props.title}</p>
+            <div className={ props.toggle ? "left-aligned toggle-wrapper" : "right-aligned toggle-wrapper"} onClick={props.toggleFunction}>
+                <ColoredSquare />
+            </div>
+        </div>
+    );
+};
 
+Toggle.propTypes = {
+    title: PropTypes.string,
+    toggle: PropTypes.bool,
+    toggleFunction: PropTypes.func
 };
 
 export default Toggle;
